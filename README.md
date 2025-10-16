c963ba7d377d28dd6a48d19a0007c3cc1e6d0336
DesarrolloAY <ymollob@gmail.com>
2025-10-16 16:44:05 -0500

    lanzamiento

---
+++ README.md
@@ -0,0 +1,3 @@
+# json_http_guia7
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/json_http_guia7/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.json_http_guia7
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
